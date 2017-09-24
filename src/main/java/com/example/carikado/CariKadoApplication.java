1cc64a140063e8adc1f59f56fd8c522ee0626a4c
Faza Zulfika P P <zulffaza@gmail.com>
2017-09-24 14:30:58 +0700

    Initialize commit

---
+++ src/main/java/com/example/carikado/CariKadoApplication.java
@@ -0,0 +1,12 @@
+package com.example.carikado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CariKadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CariKadoApplication.class, args);
+	}
+}
